2807d4dbdd95555b85fe71c45a7979ac964e438f
bpa03 <baldassarepugliese@gmail.com>
2022-06-08 13:33:23 -0400

    Added types to .env file

---
+++ server/src/types/environment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      TZ?: string;
+      PORT?: number;
+      HOSTNAME_DB?: string;
+      USER_DB?: string;
+      PASSWORD_DB?: string;
+      DB?: string;
+    }
+  }
+}
+
+export {};
