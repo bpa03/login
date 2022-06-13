3106ec3a623c31889d5fc0832c42de1b187cfe09
bpa03 <baldassarepugliese@gmail.com>
2022-06-13 13:42:19 -0400

    Added configs in .eslintrc, tsconfig and Viteconfig. Added image for form background

---
+++ client/src/GlobalStyles.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --font-family: 'Roboto', sans-serif;
+    --font-size: 18px;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-family: var(--font-family);
+    font-size: var(--font-size);
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: var(--font-family);
+    font-weight: bold;
+  }
+
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+
+`;
+
+export default GlobalStyles;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig, Alias, AliasOptions } from 'vite'
+import { defineConfig, Alias, AliasOptions } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 const Paths: Alias[] = [];
 
 const AppPath: Alias = {
   find: '@',
   replacement: path.resolve(__dirname, './src'),
-}
+};
 
-Paths.push(AppPath);
+const AssetsPath: Alias = {
+  find: 'assets',
+  replacement: path.resolve(__dirname, './assets')
+};
+
+Paths.push(AppPath, AssetsPath);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: Paths
-  }
-})
+    alias: Paths,
+  },
+});
