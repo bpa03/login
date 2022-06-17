65f7cca04a79c79e9cb0eb0c1d5025de3c049765
bpa03 <baldassarepugliese@gmail.com>
2022-06-17 12:28:23 -0400

    Basic routes protection implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { FC, Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import RequireAuth from 'app/features/RequireAuth';
 // Global Styles
 import GlobalStyles from './GlobalStyles';
 // Views
@@ -8,11 +8,15 @@ import Register from '@/views/Register';
 
 const Home = () => <h1>Home</h1>;
 const Login = () => <h1>Login</h1>;
+const DashBoard = () => <h1>Dashboard</h1>;
 
 const App: FC = () => (
   <Fragment>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<RequireAuth />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -3,13 +3,16 @@ import { FC, ReactNode } from 'react';
 // Styles
 import { ButtonField } from './styles';
 
-type Props = {
+interface Props {
   buttonType: 'submit' | 'button';
   children: ReactNode | string;
-};
+  disabled: boolean;
+}
 
-const Button: FC<Props> = ({ buttonType, children }) => (
-  <ButtonField type={buttonType}>{children}</ButtonField>
+const Button: FC<Props> = ({ buttonType, children, disabled }) => (
+  <ButtonField type={buttonType} disabled={disabled}>
+    {children}
+  </ButtonField>
 );
 
 export default Button;
